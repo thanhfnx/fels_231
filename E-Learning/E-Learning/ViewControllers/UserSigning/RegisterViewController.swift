5c15ead81eb0a945f9b791d5e683ba2592597aff
thanhfnx <thanhfnx@gmail.com>
2017-02-09 16:54:31 +0700

    update_UI_for_login_register

--- E-Learning/E-Learning/ViewControllers/UserSigning/RegisterViewController.swift
+++ E-Learning/E-Learning/ViewControllers/UserSigning/RegisterViewController.swift
@@ -10,8 +10,50 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    // MARK: - IBOutlet
+
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var retypePasswordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fullNameTextField.becomeFirstResponder()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    fileprivate func getUser() -> User? {
+        return User(fullName: self.fullNameTextField?.text,
+            email: self.emailTextField?.text,
+            password: self.passwordTextField?.text,
+            retypePassword: self.retypePasswordTextField?.text,
+            error: { (message, tag) in
+            self.show(message: message, title: nil,
+                completion: { (action) in
+                self.view.viewWithTag(tag)?.becomeFirstResponder()
+            })
+        })
+    }
+    
+    // MARK: - IBAction
+    
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        _ = self.navigationController?.popViewController(animated: true)
     }
 
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+        if let user = getUser() {
+            // TODO: Handle register
+            print(user)
+        }
+    }
+    
+    @IBAction func tapGestureRecognized(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
 }
